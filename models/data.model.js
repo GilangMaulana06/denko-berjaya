7b7fd92734b5a3fb8e2fdc06e6f0afb24ed9c9d4
GilangMaulana06 <gilangmaulana20061998@gmail.com>
2023-07-05 12:59:18 +0700

    aa

--- models/data.model.js
+++ models/data.model.js
@@ -6,12 +6,15 @@ module.exports = mongoose => {
             ukuran: String,
             type: String
         }, {
-            timestamps: true
-        }
+        timestamps: true,
+        capped: { size: 1024 },
+        bufferCommands: false,
+        autoCreate: false
+    }
     )
 
-    schema.method("toJSON", function(){
-        const {__v, _id, ...object} = this.toObject();
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
         object.id = _id
 
         return object
