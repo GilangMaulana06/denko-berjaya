b374ad08a58fb2ee9b51aac8ab160ccd9c8964fb
GilangMaulana06 <gilangmaulana20061998@gmail.com>
2023-07-07 20:37:44 +0700

    aa

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 module.exports = {
-    url: process.env.DATABASE_URL
+    url: 'mongodb+srv://gilangmaulana20061998:Gilang20061998@tokomaya.w0zf7ur.mongodb.net/tokomayapariaman?retryWrites=true&w=majority'
 }
